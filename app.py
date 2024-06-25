aa9de8c22f5e1001d01054dba42567739e6c2c6e
CarlosCifuentes <20210654@uthh.edu.mx>
2024-06-25 04:11:28 -0600

    nueva actualizacion

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, render_template, jsonify
+import joblib
+import pandas as pd
+import logging
 
 app = Flask(__name__)
 
-# Lista para almacenar las citas y sus autores
-quotes = []
+# Configurar el registro
+logging.basicConfig(level=logging.DEBUG)
 
+# Cargar el modelo entrenado
+model = joblib.load('model.pkl')
+app.logger.debug('Modelo cargado correctamente.')
+ 
 @app.route('/')
 def home():
-    return render_template('home.html', quotes=quotes)
+    return render_template('formulario.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    quote = request.form['quote']
-    author = request.form['author']
-    if quote and author:
-        quotes.append((quote, author))
-    return redirect(url_for('home'))
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Obtener los datos enviados en el request
+        year = float(request.form['year'])
+        transmission = float(request.form['transmission'])
+        engine = float(request.form['engine'])
+        seats = float(request.form['seats'])
+        
+        # Crear un DataFrame con los datos
+        data_df = pd.DataFrame([[year,transmission,engine,seats]], columns=['year', 'transmission','engine','seats'])
+        app.logger.debug(f'DataFrame creado: {data_df}')
+        
+        # Realizar predicciones
+        prediction = model.predict(data_df)
+        app.logger.debug(f'Predicción: {prediction[0]}')
+        
+        # Devolver las predicciones como respuesta JSON
+        return jsonify({'categoria': prediction[0]})
+    except Exception as e:
+        app.logger.error(f'Error en la predicción: {str(e)}')
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
